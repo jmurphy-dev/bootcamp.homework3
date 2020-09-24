380aaf457622623c76f67ac79e7c65079c4bf424
Jack Murphy <signal.space@protonmail.com>
2020-09-23 19:02:19 -0500

    prompt user for password length if button clicked

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//user parameters
+
+var parameters = {
+  //aquire password generation parameters
+  passwordLength: function()  {
+    var length = prompt("Chose a password length between 8 and 128 characters");
+
+    console.log(`Chosen length :: ${length}`);
+  },
+
+  chooseDictionary: function() {
+
+  }
+  
+
+};
+
+
+//Arrays of characters
+var dictionary = {
+  "uppercase" : [],
+  "lowerCase" : [],
+  "numbers"   : [],
+  "symbols"   : []
+};
+
+
 // Generate password
-function generatePassword() {
+function generatePassword(dictionary, parameters) {
+  parameters.passwordLength();
+  var pw = '';
+
 
   
 }
@@ -11,7 +41,7 @@ function generatePassword() {
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(dictionary, parameters);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
