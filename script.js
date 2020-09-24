54dc9fcc9808e1e0bbed637838631814dc142b07
Jack Murphy <signal.space@protonmail.com>
2020-09-23 19:50:20 -0500

    created dictionary obj of rand symbols

--- script.js
+++ script.js
@@ -9,11 +9,17 @@ var parameters = {
     var length = prompt("Chose a password length between 8 and 128 characters");
 
     console.log(`Chosen length :: ${length}`);
+
+    do {
+      alert(`Invalid choice!`);
+
+      length = prompt("Chose a password length between 8 and 128 characters");
+
+    } while((length < 8 || length > 128));
+
   },
 
-  chooseDictionary: function() {
-
-  }
+  chooseDictionary: function() {}
   
 
 };
@@ -21,16 +27,26 @@ var parameters = {
 
 //Arrays of characters
 var dictionary = {
-  "uppercase" : [],
-  "lowerCase" : [],
-  "numbers"   : [],
-  "symbols"   : []
+  randUpper: function() {
+    return String.fromCharCode(Math.floor(Math.random()*26)+65);
+  },
+  randLower: function() {
+    return String.fromCharCode(Math.floor(Math.random()*26)+97);
+  },
+  randNum: function() {
+    return String.fromCharCode(Math.floor(Math.random()*10)+48);
+  },
+  randSymbol: function() {
+    var symbol = "!@#$%^&*()_+{}[]<>?;:~`"
+    return symbol[Math.floor(Math.random()*symbol.length)];
+  }
 };
 
 
 // Generate password
 function generatePassword(dictionary, parameters) {
   parameters.passwordLength();
+  parameters.chooseDictionary();
   var pw = '';
 
 
